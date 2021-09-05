d667c051a8d83fda0bc0f25142cc5a0668edcdab
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-09-05 14:52:26 +0300

    Use database url

--- src/config/typeorm-config.service.ts
+++ src/config/typeorm-config.service.ts
@@ -10,11 +10,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: this.configService.get('DB_HOST'),
-      port: this.configService.get<number>('DB_PORT'),
-      username: this.configService.get('DB_USERNAME'),
-      password: this.configService.get('DB_PASSWORD'),
-      database: this.configService.get('DB_NAME'),
+      url: this.configService.get('DATABASE_URL'),
       entities: [join(__dirname, '..', '/**/*.entity{.ts,.js}')],
       synchronize: true,
     };
