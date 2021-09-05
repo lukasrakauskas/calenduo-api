800e9ac875539f13cd0294f39df786d75542d8d7
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-09-05 19:12:30 +0300

    Allow only admin to see all users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Roles } from '../roles/decorators/roles.decorator';
+import { Role } from '../roles/enums/role.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Roles(Role.Admin)
   index() {
     return this.usersService.findAll();
   }
