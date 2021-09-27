681df3561ec1b4da7dba98a3a0ab7a4260b37779
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-09-27 20:52:54 +0300

    Update dto validation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
+  @IsString()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
   password: string;
 }
